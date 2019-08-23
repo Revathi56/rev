96a9efcb3810e0c49e996e7d1e1f2d81f4e74b0f
harithapavan <ppavanbplkumar@gmail.com>
2019-08-23 15:06:14 +0530

    commit

---
+++ src/org/cts/test/A.java
@@ -0,0 +1,14 @@
+package org.cts.test;
+
+public class A {
+
+	public void tc1() {
+System.out.println("123456");
+	}
+public static void main(String[] args) {
+	A x = new A();
+	x.tc1();
+}
+
+
+}
---
+++ src/org/cts/test/B.java
@@ -0,0 +1,14 @@
+package org.cts.test;
+
+public class B {
+
+	public void mn() {
+System.out.println("nag");
+	}
+	
+	public static void main(String[] args) {
+	B p = new B();
+	p.mn();
+	
+	}
+}
